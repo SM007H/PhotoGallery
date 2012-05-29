3cab75299b4beeccf049bbbd5860cc0e0a39d073
SM007H <chudi.nzekwu@gmail.com>
2012-05-28 21:09:34 -0600

    Added initial functionality

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+    // Database Constants
+    defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+    defined('DB_USER') ? null : define("DB_USER", "gallery");
+    defined('DB_PASS') ? null : define("DB_PASS", "hardwork");
+    defined('DB_NAME') ? null : define("DB_NAME", "photo_gallery");
+    
+?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
+require_once( "../includes/database.php");
 
-echo "Is this working?";
+if( isset($db) ) { echo "true"; } else { echo "false"; }
+echo "<br/>";
 
+echo $db->escape_value( "It's working?<br/>");
+
+//$sql = "INSERT INTO users (id, username, password, first_name, last_name) ";
+//$sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Kevin', 'Skoglund')";
+//fetch_array$result = $db->query($sql);
+
+$sql = "SELECT * FROM users WHERE id = 1";
+$result_set = $database->query( $sql );
+$found_user = $database->fetch_array( $result_set );
+echo $found_user['username'];
 ?>
